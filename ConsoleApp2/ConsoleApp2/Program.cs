59b5343b0a5ee7b4ea29f17d2056768caf2b9d4a
Andreas Gruenwald <violator2003@gmail.com>
2022-08-18 21:58:28 +0200

    erste

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;// See https://aka.ms/new-console-template for more information
+namespace ConsoleApp2
+{
+    class Programm {
+        static int X (string label, int i)
+        {
+            Console.Write(label);
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(X("a",1) + X("b", 1) + X("c", 1) + X("d", 1) );
+        }
+          
+
+}
+   
+
+}
+
+
---
+++ Project1/Project1/CodeFile1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+class OddEvenDemo
+{
+    static void Main() {
+        int number, reminder;
+
+    number=Int32.Parse(
+
+        Interaction.InputBox(
+        "Input name: ","Prüfung"
+        )
+        );
+        
+        reminder = number%2;
+        string txt = "Sie ausfühlen";
+
+    txt+= (reminder==0?"Chetnoe":"Nechetnoe")+" chislo!";
+        MessageBox.Show(txt);
+        }
+        }
